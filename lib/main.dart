d843114ffcc90ab2bb11405463996c0e695746b1
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-13 20:28:43 -0300

    Implemented two columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(
-    Column(
-      children: const <Widget>[
-        Text('Deliver features faster', textDirection: TextDirection.ltr,),
-        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
-
-      ],
-    ),
-  );
-}
+void main() => runApp(
+  Column(
+    children: <Widget>[
+      const Text(
+        'Deliver features faster',
+        textDirection: TextDirection.ltr,
+      ),
+      const Text(
+        'Craft beautiful UIs',
+        textDirection: TextDirection.ltr,
+      ),
+      const Text(
+        'Craft beautiful UIs',
+        textDirection: TextDirection.ltr,
+      ),
+      Column(children: const <Widget>[
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+      ])
+    ],
+  ), //Column
+);
