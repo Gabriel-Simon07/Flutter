df1a204193e36b8b55ef16cc3e2e380c96cbc180
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-13 18:21:19 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Column(
+      children: const <Widget>[
+        Text('Deliver features faster', textDirection: TextDirection.ltr,),
+        Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+
+      ],
+    ),
+  );
+}
