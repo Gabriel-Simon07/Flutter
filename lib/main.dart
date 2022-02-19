fdd66ac9c76462eda0ed0eff95d00ab2a2dd75db
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-19 18:50:39 -0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,53 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(
-  Column(
-    children: <Widget>[
-      const Text(
-        'Deliver features faster',
-        textDirection: TextDirection.ltr,
+  MaterialApp(
+    home: Scaffold(
+      body: ListaTransferencias(),
+      appBar: AppBar(title: Text('Transfêrencias'),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
       ),
-      const Text(
-        'Craft beautiful UIs',
-        textDirection: TextDirection.ltr,
-      ),
-      const Text(
-        'Craft beautiful UIs',
-        textDirection: TextDirection.ltr,
-      ),
-      Column(children: const <Widget>[
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-      ])
-    ],
-  ), //Column
+    ),
+  )
 );
+
+class ListaTransferencias extends StatelessWidget {
+
+@override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencia(Transferencia(1000.0, 12463)),
+        ItemTransferencia(Transferencia(2000.0, 23463)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+
+final Transferencia _transferencia;
+
+ ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numeroConta.toString()),
+        )
+    );
+  }
+}
+
+class Transferencia {
+
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
