93adf66ac3029f294424cff23b3e5221f51ac6ac
Gabriel Simon <gabrielsimon775@gmail.com>
2022-03-01 15:44:02 -0300

    Button add transfers implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 
@@ -69,7 +68,41 @@ class FormularioTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Criado transferência'),),
-        body: Text('teste'));
+      appBar: AppBar(title: Text('Criando transferência'),),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                 fontSize: 20.0
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000'
+                ),
+                  keyboardType: TextInputType.number,
+              ),
+            ),Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                 fontSize: 20.0
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0.00'
+                ),
+                  keyboardType: TextInputType.number,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => ListaTransferencias(),
+                child: Text(
+              'Confirmar'
+            ))
+          ],
+        ));
   }
 }
