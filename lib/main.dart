8b2c483b4df846f306a090f24e6d70213243f33b
Gabriel Simon <gabrielsimon775@gmail.com>
2022-03-02 21:21:49 -0300

    Transfer completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(ByteBankApp());
@@ -17,11 +16,12 @@ class ByteBankApp extends StatelessWidget {
 }
 
 class ListaTransferencias extends StatelessWidget {
-
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Transfêrencias'),),
+      appBar: AppBar(
+        title: Text('Transfêrencias'),
+      ),
       body: Column(
         children: [
           ItemTransferencia(Transferencia(1000.0, 12463)),
@@ -29,79 +29,91 @@ class ListaTransferencias extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
   }
 }
 
-class ItemTransferencia extends StatelessWidget{
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
 
-final Transferencia _transferencia;
-
- ItemTransferencia(this._transferencia);
+  ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text(_transferencia.valor.toString()),
-          subtitle: Text(_transferencia.numeroConta.toString()),
-        )
-    );
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
   }
 }
 
 class Transferencia {
-
   final double valor;
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
 
 class FormularioTransferencia extends StatelessWidget {
-  const FormularioTransferencia({Key? key}) : super(key: key);
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValorConta =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Criando transferência'),),
+        appBar: AppBar(
+          title: Text('Criando transferência'),
+        ),
         body: Column(
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
-                style: TextStyle(
-                 fontSize: 20.0
-                ),
+                controller: _controladorCampoNumeroConta,
+                style: TextStyle(fontSize: 20.0),
                 decoration: InputDecoration(
-                  labelText: 'Número da conta',
-                  hintText: '0000'
-                ),
-                  keyboardType: TextInputType.number,
+                    labelText: 'Número da conta', hintText: '0000'),
+                keyboardType: TextInputType.number,
               ),
-            ),Padding(
+            ),
+            Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
-                style: TextStyle(
-                 fontSize: 20.0
-                ),
+                controller: _controladorCampoValorConta,
+                style: TextStyle(fontSize: 20.0),
                 decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor',
-                  hintText: '0.00'
-                ),
-                  keyboardType: TextInputType.number,
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0.00'),
+                keyboardType: TextInputType.number,
               ),
             ),
             ElevatedButton(
-              onPressed: () => ListaTransferencias(),
-                child: Text(
-              'Confirmar'
-            ))
+                onPressed: () {
+                  debugPrint('Clicou no botão de confirmar');
+                  final int? numeroConta =
+                      int.tryParse(_controladorCampoNumeroConta.text);
+                  final double? valorConta =
+                      double.tryParse(_controladorCampoValorConta.text);
+                  if (numeroConta != null && valorConta != null) {
+                    final transferenciaCriada =
+                        Transferencia(valorConta, numeroConta);
+                    debugPrint('$transferenciaCriada');
+                  }
+                },
+                child: Text('Confirmar'))
           ],
         ));
   }
